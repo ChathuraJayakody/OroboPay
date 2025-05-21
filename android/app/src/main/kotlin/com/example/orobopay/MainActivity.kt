f07a088c74c705a4a65ff0ba10450bfc4d407b1f
Chathura Jayakody <chathurajy96@gmail.com>
2025-05-21 19:07:16 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/orobopay/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.orobopay
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/Screens/home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+
+
+    final homeBody = SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 12),
+            BalanceCard(country: settings.currencyCountry),
+          ],
+        ),
+      ),
+    );
+
+    return Scaffold(
+
+    );
+  }
+}
